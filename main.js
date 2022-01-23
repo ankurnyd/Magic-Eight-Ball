a8db59e6f2ab5065939520559b3ca07aae00b2f9
Ankur Sharma <38402246+ankurnyd@users.noreply.github.com>
2022-01-24 05:20:31 +0530

    Completed
    OUTPUT
    Hello, Ankur
    The user asked: How is my life
    It is certain

---
+++ main.js
@@ -0,0 +1,39 @@
+let userName ='Ankur';
+userName? console.log(`Hello, ${userName}`)
+:console.log('Hello!');
+
+const userQuestion = "How is my life";
+console.log (`The user asked: ${userQuestion}`);
+const randomNumber= Math.floor(Math.random()*8);
+let eightBall=1;
+
+switch(eightBall){
+  case 1:
+  console.log('It is certain');
+  break;
+  case 2:
+  console.log('It is decidedly so');
+  break;
+  case 3:
+  console.log('Reply hazy try again');
+  break;
+  case 4:
+  console.log('Cannot predict now');
+  break;
+  case 5:
+  console.log('Do not count on it');
+  break;
+  case 6:
+  console.log('My sources say no');
+  break;
+  case 7:
+  console.log('Outlook not so good');
+  break;
+  case 8:
+  console.log('Signs point to yes');
+  break;
+  default:
+  console.log('Enter the number');
+  break;
+}
+
